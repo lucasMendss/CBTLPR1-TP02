f77aff160de64a38add4fda38a5a8d8a6d32d7c7
felipe <167010204+30felipedev@users.noreply.github.com>
2026-04-15 00:13:46 -0300

    tp02ex08
    added a Main class to read a matrix and multiply it by a constant.

--- src/main/java/org/example/TP02Ex08.java
+++ src/main/java/org/example/TP02Ex08.java
@@ -1,4 +1,45 @@
 package org.example;
 
 public class TP02Ex08 extends Exercicio{
+
+  public class Main {
+      public static void main(String[] args) {
+          double[][] matriz = new double[3][4];
+          double[][] resultado = new double[3][4];
+          int c = 1;
+
+          System.out.println("Digite 12 valores: ");
+          for (int i = 0; i < 3; i++) {
+              for (int j = 0; j < 4; j++) {
+                  System.out.print(c + "° valor: ");
+                  matriz[i][j] = scanner.nextDouble();
+                  c++;
+              }
+          }
+
+          System.out.print("Digite a constante multiplicativa: ");
+          double constante = scanner.nextDouble();
+
+          for (int i = 0; i < 3; i++) {
+              for (int j = 0; j < 4; j++) {
+                  resultado[i][j] = matriz[i][j] * constante;
+              }
+          }
+
+          System.out.println("\nValores originais:");
+          imprimirMatriz(matriz);
+          System.out.println("\nValores resultantes:");
+          imprimirMatriz(resultado);
+
+      }
+
+      private static void imprimirMatriz(double[][] m) {
+          for (int i = 0; i < m.length; i++) {
+              for (int j = 0; j < m[i].length; j++) {
+                  System.out.printf("%10.2f", m[i][j]);
+              }
+              System.out.println();
+          }
+      }
+  }
 }
